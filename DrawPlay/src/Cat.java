71b3054ca7480bb5be46be5db9488ca1758f5651
jackpender <20085278@mail.wit.ie>
2019-02-15 14:59:34 +0000

    This Cat is a joke!

--- DrawPlay/src/Cat.java
+++ DrawPlay/src/Cat.java
@@ -37,14 +37,14 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.red);
+		g2.setColor(Color.blue);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.orange);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("meow", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
 
